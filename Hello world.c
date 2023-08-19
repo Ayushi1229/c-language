db368cd50904ec067a88f6e902cfe75b84fb8122
Ayushi1229 <140989912+Ayushi1229@users.noreply.github.com>
2023-08-19 09:17:27 +0530

    Add files via upload

---
+++ Hello world.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	print("Enter an integer val: ");
+	scanf("%d",&a);
+	printf("Enter an integer val: ");
+	scaf("%d",&b);
+	c=a+b;
+	printf("Sum= %d",c);
+}
